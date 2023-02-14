1dfa82c1810061995eb6806e523fb8a31ed19b29
Noman Mahmud <molla.noman23@gmail.com>
2023-02-14 19:28:55 +0600

    feat: add BookTable component

--- client-app/components/SectionHeader.jsx
+++ client-app/components/SectionHeader.jsx
@@ -1,8 +1,8 @@
 const SectionHeader = ({ upperText, lowerText }) => {
   return (
-    <div className='text-center mb-7 md:mb-16 '>
-      <h1 className='text-golden text-2xl sm:text-5xl italic'>{upperText}</h1>
-      <h1 className='text-2xl sm:text-5xl text-slate-900 font-semibold'>
+    <div className="text-center mb-7 md:mb-16 ">
+      <h1 className="text-golden text-2xl  md:text-5xl italic">{upperText}</h1>
+      <h1 className="text-2xl  md:text-5xl text-slate-900 font-semibold">
         {lowerText}
       </h1>
     </div>
