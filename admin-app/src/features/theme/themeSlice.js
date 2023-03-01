61167b4091728bfc75a5d792d304ddf7d89b2556
Rakibul Islam <rakibul.islam7772588@gmail.com>
2023-03-01 22:59:19 +0600

    [FIX] theme store in local storage

--- admin-app/src/features/theme/themeSlice.js
+++ admin-app/src/features/theme/themeSlice.js
@@ -1,8 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-	lightMode: true,
-};
+const themeStateFromLocalStorage = localStorage.getItem(
+	"_restaurant_management_theme"
+)
+	? JSON.parse(localStorage.getItem("_restaurant_management_theme"))
+	: null;
+
+const initialState = themeStateFromLocalStorage
+	? themeStateFromLocalStorage
+	: { lightMode: true };
 
 const themeSlice = createSlice({
 	name: "theme",
@@ -10,6 +16,11 @@ const themeSlice = createSlice({
 	reducers: {
 		toggleLightMode: (state, action) => {
 			state.lightMode = !state.lightMode;
+
+			localStorage.setItem(
+				"_restaurant_management_theme",
+				JSON.stringify(state)
+			);
 		},
 	},
 });
